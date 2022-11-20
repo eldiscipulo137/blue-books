9a8e834aab9f45c83547c52ac99c3be7d0721685
eldiscipulo137 <eldiscipulo137@gmail.com>
2022-11-20 15:24:59 -0500

    update title and tagline

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,8 +6,8 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: "Blue Books",
-  tagline: "Alice Bailey - Blue books",
+  title: "Discipulo",
+  tagline: "Información util para el discipulado",
   url: "https://your-docusaurus-test-site.com",
   baseUrl: "/",
   onBrokenLinks: "throw",
