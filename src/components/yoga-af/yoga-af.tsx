46c968e339486318a01d7f60964e0a3ddfb2ec40
eldiscipulo137 <eldiscipulo137@gmail.com>
2022-12-01 08:11:48 -0500

    se ajusta los links

--- src/components/yoga-af/yoga-af.tsx
+++ src/components/yoga-af/yoga-af.tsx
@@ -1,3 +1,4 @@
+import Link from "@docusaurus/Link";
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { FC } from "react";
@@ -10,9 +11,9 @@ export interface YogaAfProps {
 const YogaAf: FC<YogaAfProps> = ({ book, af }) => {
   return (
     <>
-      <a href={`/la-luz-del-alma/libro${book}#af${af}`}>
+      <Link to={`/la-luz-del-alma/libro${book}#af${af}`}>
         <FontAwesomeIcon icon={faArrowRight} />
-      </a>
+      </Link>
     </>
   );
 };
